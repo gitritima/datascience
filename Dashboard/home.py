cbe2d9212f0234224958d44ef79f557a7432fd74
Ritima Sharma <ritimasharma@gmail.com>
2025-02-05 18:44:54 +0530

    new changes

--- Dashboard/home.py
+++ Dashboard/home.py
@@ -4,4 +4,5 @@ import seaborn as sns
 import plotly.express as ps
 import plotly.graph_objects as go
 
-st.title("Titanic Data Analysis")
+st.markdown('<h1 style="color:white; text-align:centre; border:2px solid white; background-color:maroon; font-family:sans sarif; margin-bottom:20px; padding-left:90px;">Titanic Data Analysis</h1>',unsafe_allow_html=True)
+st.image('') #dashed border 
--- Dashboard/pages/dashboard.py
+++ Dashboard/pages/dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
-import plotly.express as ps
+import plotly.express as px
 import plotly.graph_objects as go
 
 st.title("Titanic Data Analysis")
@@ -10,3 +10,36 @@ df=sns.load_dataset("titanic")
 
 st.dataframe(df)
 
+st.sidebar.header("Filter Options")
+#gender filter
+gender=st.sidebar.multiselect('Gender',
+                           options=df['sex'].unique(),
+                           default=df['sex'].unique())
+
+#class filter
+pclass=st.sidebar.multiselect('class',
+                              options=sorted(df['pclass'].unique()),
+                              default=sorted(df['pclass'].unique()))
+#age filter
+min_age, max_age=st.sidebar.slider('age',
+                                  min_value=int(df['age'].min()),
+                                  max_value=int(df['age'].max()),
+                                   value = (int(df['age'].min()), int(df['age'].max())))
+
+#filter the dataset based on selection
+filtered_data=df[
+    (df['sex'].isin(gender))&
+    (df['pclass'].isin(pclass))&
+    (df['age']>=min_age)&
+    (df['age']<=max_age)]
+
+st.subheader("Gender Distribution")
+gender_count = filtered_data['sex'].value_counts()
+fig = px.pie(names=gender_count.index, values=gender_count.values, title = "Gender Distribution")
+st.plotly_chart(fig)
+
+#Create a histogram of age distribution
+st.subheader("Age Distribution")
+fig = px.histogram(filtered_data, x='age', nbins=20, title = "Age Distribution", 
+        labels = {'age': 'Age', 'count': 'Number of Passengers'})
+st.plotly_chart(fig)
